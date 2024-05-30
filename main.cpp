a55b004d44fd300b6d23affc5c9b4e8428294e35
chyves <82107846+notchyves@users.noreply.github.com>
2024-05-29 21:02:41 -0400

    no api wow

--- main.cpp
+++ main.cpp
@@ -28,21 +28,6 @@ DWORD WINAPI initialize(HMODULE instance){
     ImGui::ShowDemoWindow();
 
 
-
-    std::string json_string = Utils::DownloadString("http://node2.synthetix.host:5004/blocked");
-    try {
-        json j = json::parse(json_string);
-
-        for (json::iterator it = j.begin(); it != j.end(); ++it) {
-            const std::string& key = it.key();
-            std::vector<std::string> v = j[key];
-            ModMenu::blocked_modules[key] = v;
-        }
-    } catch (json::parse_error& e) {
-        Logger::info(e.what());
-
-    }
-
     std::thread statusThread([&lastBeatTime]() {
         while (true) {
             auto now = std::chrono::steady_clock::now();
@@ -59,47 +44,6 @@ DWORD WINAPI initialize(HMODULE instance){
                             Client::partners.clear();
                             std::string name = SDK::clientInstance->getLocalPlayer()->playerName;
                             //Client::onlineUsers.push_back(Utils::removeColorCodes(name));
-
-                            std::string ipToSend;
-
-                            Logger::info(Utils::DownloadString(xorstr("http://node2.synthetix.host:5004/heartbeat/") + SDK::getServerIP() + "/" + Utils::removeColorCodes(name)));
-                            std::string pp = Utils::DownloadString(xorstr("http://node2.synthetix.host:5004/users"));
-                            if(pp != "") {
-                                json playersDict = json::parse(pp);
-
-                                int totalPlaytime = 0;
-                                int numberOfPlayers = 0;
-
-                                for (const auto &player: playersDict.items()) {
-
-                                    std::time_t unixTimestamp = player.value()["lastbeat"];
-                                    std::time_t timePoint = std::chrono::system_clock::to_time_t(std::chrono::system_clock::from_time_t(unixTimestamp));
-                                    Client::online.push_back(Utils::removeColorCodes(player.key()));
-                                    std::string icon = player.value()["icon"];
-                                    Client::PlayerServerList[Utils::removeColorCodes(player.key())] = player.value()["server"];
-                                    if (icon == "regular") {
-                                        Client::onlineUsers.push_back(Utils::removeColorCodes(player.key()));
-                                    }
-                                    if (icon == "staff") {
-
-                                        Client::onlineStaff.push_back(Utils::removeColorCodes(player.key()));
-                                    }
-                                    if (icon == "staffplus") {
-
-                                        Client::onlineStaffp.push_back(Utils::removeColorCodes(player.key()));
-                                    }
-                                    if (icon == "partner") {
-
-                                        Client::partners.push_back(Utils::removeColorCodes(player.key()));
-                                    }
-
-
-                                }
-                                Client::HeartBeatFriend();
-                            }else{
-                                notifs.push_back("Error while trying to connect to the api");
-                            }
-                            lastBeatTime = now;
                         }
                     }
                 }
